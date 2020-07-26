fe22663e63825629ffdac8a2fca97abc0f7f5a43
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-26 15:26:01 -0700

    INPUT schema/sql data and ran in workbench, both successful

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('VeganBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
